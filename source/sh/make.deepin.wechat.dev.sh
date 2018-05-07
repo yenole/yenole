861024ab83e7c049194f0372878570210c366c9e
Yenole <Netxy@vip.qq.com>
2018-05-07 17:33:08 +0800

    update

--- source/sh/make.deepin.wechat.dev.sh
+++ source/sh/make.deepin.wechat.dev.sh
@@ -8,8 +8,21 @@ fi
 # 获取执行sudo命令
 sudo ls >/dev/null
 
-# 复制files.7z和更新TIM文件
+# 解决远程调试问题
+mkdir /tmp/node-sync-ipc
+cd /tmp/node-sync-ipc
+yarn add node-sync-ipc@2.1.2
+
 cd ~/.deepinwine/Deepin-WeChatDev/
+rm -rf drive_c/Program\ Files/WeChat_nwjs/package.nw/node_modules/node-sync-ipc
+rm -rf drive_c/Program\ Files/WeChat_nwjs/package.nw/node_modules/node-sync-ipc-nwjs
+cp -r /tmp/node-sync-ipc/node_modules/node-sync-ipc drive_c/Program\ Files/WeChat_nwjs/package.nw/node_modules/node-sync-ipc
+cp -r /tmp/node-sync-ipc/node_modules/node-sync-ipc drive_c/Program\ Files/WeChat_nwjs/package.nw/node_modules/node-sync-ipc-nwjs
+
+cp `type node | awk '{print $3}'` drive_c/Program\ Files/WeChat_nwjs/node.exe
+rm -rf /tmp/node-sync-ipc
+
+# 复制files.7z和更新TIM文件
 cp $dir/files.7z ./files.7z
 7z d files.7z drive_c/Program\ Files/WeChat_nwjs/package.nw/
 7z a files.7z drive_c/Program\ Files/WeChat_nwjs/package.nw/
